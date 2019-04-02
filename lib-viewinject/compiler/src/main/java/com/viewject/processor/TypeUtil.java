2c9c0b09e4abc519be6524c1bfdd7ba43dd41ebd
liuk <471636288@qq.com>
2019-04-02 10:35:25 +0800

    修改note

--- lib-viewinject/compiler/src/main/java/com/viewject/processor/TypeUtil.java
+++ lib-viewinject/compiler/src/main/java/com/viewject/processor/TypeUtil.java
@@ -2,11 +2,6 @@ package com.viewject.processor;
 
 import com.squareup.javapoet.ClassName;
 
-/**
- * Created by mingwei on 12/15/16.
- * CSDN:    http://blog.csdn.net/u013045971
- * Github:  https://github.com/gumingwei
- */
 public class TypeUtil {
     public static final ClassName FINDER = ClassName.get("com.viewinject.bindview.finder", "Finder");
     public static final ClassName VIEWCLICKER = ClassName.get("com.viewinject.bindview.finder", "ViewClicker");
