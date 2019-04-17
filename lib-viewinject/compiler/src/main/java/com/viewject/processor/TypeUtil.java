03b332c01431d05a9ea575cb1b79e83ad06aa8d9
liuk <471636288@qq.com>
2019-04-17 17:34:42 +0800

    增加流程图

--- lib-viewinject/compiler/src/main/java/com/viewject/processor/TypeUtil.java
+++ lib-viewinject/compiler/src/main/java/com/viewject/processor/TypeUtil.java
@@ -4,7 +4,6 @@ import com.squareup.javapoet.ClassName;
 
 public class TypeUtil {
     public static final ClassName FINDER = ClassName.get("com.viewinject.bindview.finder", "Finder");
-    public static final ClassName VIEWCLICKER = ClassName.get("com.viewinject.bindview.finder", "ViewClicker");
     public static final ClassName VIEWINJECTOR = ClassName.get("com.viewinject.bindview", "ViewInjector");
     public static final ClassName ANDROID_ONCLICK_LISTENER = ClassName.get("android.view", "View", "OnClickListener");
     public static final ClassName ANDROID_VIEW = ClassName.get("android.view", "View");
