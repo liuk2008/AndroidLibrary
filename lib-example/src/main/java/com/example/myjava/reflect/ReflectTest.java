d36d84c6342ebb99cdde22962efb25f7b8ca69e7
liuk2008 <471636288@qq.com>
2020-10-09 15:23:47 +0800

    修改bintray平台版本

--- lib-example/src/main/java/com/example/myjava/reflect/ReflectTest.java
+++ lib-example/src/main/java/com/example/myjava/reflect/ReflectTest.java
@@ -117,7 +117,7 @@ public class ReflectTest {
         Method func = clazz.getDeclaredMethod("func1", String[].class);
         func.setAccessible(true);
         // 注意：可变参数，这个变量其实是数组，它会自动把多个参数组装成一个数组
-        func.invoke(test, new String[1]);// 运行正常：null，将可变参数拆解，长度为1，但是没有元素，所以null
+//        func.invoke(test, new String[1]);// 运行正常：null，将可变参数拆解，长度为1，但是没有元素，所以null
         func.invoke(test, (Object) new String[]{"a", "b"}); // 运行正常：2
         func.invoke(test, new Object[]{new String[]{"a", "b", "c"}}); // 运行正常：3
 //        func.invoke(test, new String[]{"a"}); // 报错：将可变参数拆解后，入参为String-->a，与func1入参String[]类型不符合
