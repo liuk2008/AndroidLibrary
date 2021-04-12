6777e6395b396a1126805b96c6d8a383f076ecb5
liuk2008 <471636288@qq.com>
2021-04-12 15:33:12 +0800

    修改compiler包

--- lib-viewinject/compiler/src/main/java/com/viewinject/processor/TypeUtil.java
+++ lib-viewinject/compiler/src/main/java/com/viewinject/processor/TypeUtil.java
@@ -7,5 +7,5 @@ public class TypeUtil {
     public static final ClassName VIEWINJECTOR = ClassName.get("com.viewinject.bindview", "ViewInjector");
     public static final ClassName ANDROID_ONCLICK_LISTENER = ClassName.get("android.view", "View", "OnClickListener");
     public static final ClassName ANDROID_VIEW = ClassName.get("android.view", "View");
-    public static final ClassName ANDROID_ANNOTATION_UITHREAD = ClassName.get("android.support.annotation", "UiThread");
+    public static final ClassName ANDROID_ANNOTATION_UITHREAD = ClassName.get("androidx.annotation", "UiThread");
 }
