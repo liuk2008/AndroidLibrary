32c510b1547f894d9f035b886a3eddb23444ebf5
liuk2008 <471636288@qq.com>
2021-04-12 15:25:10 +0800

    主项目测试viewinject

--- app/src/main/java/com/android/library/MainActivity.java
+++ app/src/main/java/com/android/library/MainActivity.java
@@ -1,9 +1,14 @@
 package com.android.library;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.android.library.database.DatabaseDao;
@@ -15,6 +20,8 @@ import com.android.network.header.MyCookieManager;
 import com.android.network.header.MyHeaderManager;
 import com.android.network.http.engine.HttpEngine;
 import com.android.network.retrofit.RetrofitEngine;
+import com.viewinject.annotation.MyBindView;
+import com.viewinject.bindview.MyViewInjector;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,10 +31,23 @@ public class MainActivity extends AppCompatActivity {
     private String token = "";
     private RetrofitDemo retrofitDemo;
 
+    @MyBindView(R.id.btn_view)
+    Button btn_view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 测试APT机制
+        MyViewInjector.bindView(this);
+        btn_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "btn_view", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         // 添加Header
         MyHeaderManager myHeaderManager = MyHeaderManager.getInstance();
         myHeaderManager.addHeader("version", "1.0");
@@ -75,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         if (retrofitDemo != null)
             retrofitDemo.cancelAll();
+        MyViewInjector.unbindView(this);
     }
 
     private void testHttp() {
